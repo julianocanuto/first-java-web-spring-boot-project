2457882e0b18e7020c239316ea6c13c333b0e50b
julianocanuto <julianocanuto@gmail.com>
2021-08-26 15:33:04 -0300

    Project setup completed

---
+++ src/main/java/br/com/julianocanuto/myfirstproject/MyfirstprojectApplication.java
@@ -0,0 +1,13 @@
+package br.com.julianocanuto.myfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/julianocanuto/myfirstproject/MyfirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.julianocanuto.myfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
