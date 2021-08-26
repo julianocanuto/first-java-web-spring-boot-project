13521cd3bc21d007c53ef43d0fc5a005d219b79e
julianocanuto <julianocanuto@gmail.com>
2021-08-26 16:38:14 -0300

    Add CategoryResource

---
+++ src/main/java/br/com/julianocanuto/myfirstproject/resources/CategoryResource.java
@@ -0,0 +1,25 @@
+package br.com.julianocanuto.myfirstproject.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.julianocanuto.myfirstproject.entities.Category;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+		List<Category> list = new ArrayList<>();
+		list.add(new Category(1L, "Electronics"));
+		list.add(new Category(2L, "Books"));
+		return ResponseEntity.ok().body(list);
+	}
+
+}
