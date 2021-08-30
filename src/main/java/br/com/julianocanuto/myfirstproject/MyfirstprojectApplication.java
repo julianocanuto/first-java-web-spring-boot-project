59aca6b01b189b10ff00b20202a031d7f30a9b42
Juliano Canuto <julianocanuto@gmail.com>
2021-08-30 18:58:51 -0300

    Add hardcoded categories in CommandLineRunner

--- src/main/java/br/com/julianocanuto/myfirstproject/MyfirstprojectApplication.java
+++ src/main/java/br/com/julianocanuto/myfirstproject/MyfirstprojectApplication.java
@@ -20,7 +20,11 @@ public class MyfirstprojectApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		Category cat1 = new Category(1L, "Electronics");
+		Category cat2 = new Category(2L, "Books");
 
+		categoryRepository.save(cat1);
+		categoryRepository.save(cat2);
 	}
 
 }
