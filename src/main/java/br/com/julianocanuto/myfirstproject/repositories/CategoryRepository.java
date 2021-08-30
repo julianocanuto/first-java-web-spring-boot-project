1c9368f00fa34d97b195c1eeee1b867c865573e2
Juliano Canuto <julianocanuto@gmail.com>
2021-08-30 18:41:29 -0300

    Add category repository that store using Map collection

---
+++ src/main/java/br/com/julianocanuto/myfirstproject/repositories/CategoryRepository.java
@@ -0,0 +1,30 @@
+package br.com.julianocanuto.myfirstproject.repositories;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import br.com.julianocanuto.myfirstproject.entities.Category;
+
+@Component
+public class CategoryRepository {
+	
+	private Map<Long, Category> map = new HashMap<>();
+
+	public void save(Category obj) {
+		map.put(obj.getId(), obj);
+	}
+
+	public Category findById(Long id) {
+		return map.get(id);
+	}
+
+	public List<Category> findAll() {
+
+		return new ArrayList<Category>(map.values());
+	}
+
+}
--- src/main/java/br/com/julianocanuto/myfirstproject/resources/CategoryResource.java
+++ src/main/java/br/com/julianocanuto/myfirstproject/resources/CategoryResource.java
@@ -1,8 +1,8 @@
 package br.com.julianocanuto.myfirstproject.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,22 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.julianocanuto.myfirstproject.entities.Category;
+import br.com.julianocanuto.myfirstproject.repositories.CategoryRepository;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Electronics"));
-		list.add(new Category(2L, "Books"));
+		List<Category> list = categoryRepository.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Category> findById(@PathVariable Long id){
-		Category cat = new Category(25L, "Iphone XXS");
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+		Category cat = categoryRepository.findById(id);
 		return ResponseEntity.ok().body(cat);
 	}
 
