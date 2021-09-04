20e2da62ac4c9fc31e552004417c862f634e93e7
Juliano Canuto <julianocanuto@gmail.com>
2021-09-04 14:39:22 -0300

    Use JPARepository to save data in database

--- src/main/java/br/com/julianocanuto/myfirstproject/repositories/CategoryRepository.java
+++ src/main/java/br/com/julianocanuto/myfirstproject/repositories/CategoryRepository.java
@@ -1,30 +1,11 @@
 package br.com.julianocanuto.myfirstproject.repositories;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.julianocanuto.myfirstproject.entities.Category;
 
-@Component
-public class CategoryRepository {
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 	
-	private Map<Long, Category> map = new HashMap<>();
-
-	public void save(Category obj) {
-		map.put(obj.getId(), obj);
-	}
-
-	public Category findById(Long id) {
-		return map.get(id);
-	}
-
-	public List<Category> findAll() {
-
-		return new ArrayList<Category>(map.values());
-	}
-
 }
--- src/main/java/br/com/julianocanuto/myfirstproject/resources/ProductResource.java
+++ src/main/java/br/com/julianocanuto/myfirstproject/resources/ProductResource.java
@@ -27,7 +27,7 @@ public class ProductResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Product> findById(@PathVariable Long id) {
-		Product cat = productRepository.findById(id);
+		Product cat = productRepository.findById(id).get();
 		return ResponseEntity.ok().body(cat);
 	}
 
