c9a8e1d4da8ea652c41fb84acbb77a5336e2496b
Juliano Canuto <julianocanuto@gmail.com>
2021-09-04 12:27:57 -0300

    Update hardcoded addition of categories and product considering their association

--- src/main/java/br/com/julianocanuto/myfirstproject/MyfirstprojectApplication.java
+++ src/main/java/br/com/julianocanuto/myfirstproject/MyfirstprojectApplication.java
@@ -1,11 +1,14 @@
 package br.com.julianocanuto.myfirstproject;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.julianocanuto.myfirstproject.entities.Category;
+import br.com.julianocanuto.myfirstproject.entities.Product;
 import br.com.julianocanuto.myfirstproject.repositories.CategoryRepository;
 
 @SpringBootApplication
@@ -22,6 +25,15 @@ public class MyfirstprojectApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Category cat1 = new Category(1L, "Electronics");
 		Category cat2 = new Category(2L, "Books");
+
+		Product p1 = new Product(1L, "TV", 2200.00, cat1);
+		Product p2 = new Product(2L, "Domain Driven Design", 120.00, cat2);
+		Product p3 = new Product(3L, "PS5", 2800.00, cat1);
+		Product p4 = new Product(4L, "Docker", 100.00, cat2);
+
+		cat1.getProducts().addAll(Arrays.asList(p1, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2, p4));
+
 
 		categoryRepository.save(cat1);
 		categoryRepository.save(cat2);
